2fad6ee832fcde1c7c37e9af98a0b20e4be9f209
RominaMalimban <romina.lyn@hotmail.it>
2023-02-14 15:43:45 +0100

    creazione colonne migrations

--- database/migrations/2023_02_14_141440_create_categories_table.php
+++ database/migrations/2023_02_14_141440_create_categories_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+
+            $table->string('code', 5) -> unique();
+            $table->string('name', 32);
+            $table->text('description') -> nullable();
+            
             $table->timestamps();
         });
     }
