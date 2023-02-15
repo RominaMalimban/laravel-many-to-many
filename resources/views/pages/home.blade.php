73e00fa3624d44d9fecaf87e97324195c3051a4b
RominaMalimban <romina.lyn@hotmail.it>
2023-02-15 01:31:41 +0100

    stampa su pagina

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,4 +2,24 @@
 
 @section('content')
     <h1>Products</h1>
+
+    @foreach ($categories as $category)
+        <h2>Category: {{$category -> name}}</h2>
+
+        <ul>
+            @foreach ($category -> products as $product)
+                <li>
+                    <div>
+                        <strong>Products name:</strong> {{$product -> name}} 
+                    </div>  
+                    <div>
+                        <strong>Typology name:</strong> {{$product -> typology -> name}} 
+                    </div> 
+                    <div>
+                        <strong>Digital:</strong> {{$product -> typology -> digital ? "Yes" : "No"}}
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    @endforeach
 @endsection
