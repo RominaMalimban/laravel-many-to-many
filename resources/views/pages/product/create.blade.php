3c6e503593d6810e0d148f468f15314a6847f69e
RominaMalimban <romina.lyn@hotmail.it>
2023-02-15 12:13:09 +0100

    creazione form con labels e input

--- resources/views/pages/product/create.blade.php
+++ resources/views/pages/product/create.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>Create a new product</h1>
+
+    <div class="container">
+        <h1>Create a new product</h1>
+
+        <form method="POST">
+            <div class="form-group">
+                <label for="name">Enter the name of the product</label>
+                <input type="text" class="form-control" name="name">
+            </div>
+            <div class="form-group">
+                <label for="description">Enter a description of the product</label>
+                <input type="text" class="form-control" name="description">
+            </div>
+            <div class="form-group">
+                <label for="price">Enter the price of the product</label>
+                <input type="number" class="form-control" name="price">
+            </div>
+            <div class="form-group">
+                <label for="weight">Enter the weight of the product</label>
+                <input type="number" class="form-control" name="weight">
+            </div>
+
+            <input type="submit" class="btn btn-primary" value="Create">
+        </form>
+    </div>
+    
 @endsection
